515cfe8b5988d9efc8f963bbafd0b90beb76056d
Tayyba Zulfiqar <tayybazulfiqar.2004@gmail.com>
2025-07-28 19:07:39 +0500

    fixed navigation logic in 404 page

--- Frontend/src/Pages/PageNotFound/NotFound.jsx
+++ Frontend/src/Pages/PageNotFound/NotFound.jsx
@@ -1,8 +1,20 @@
 import "./NotFound.css";
 import { useNavigate } from "react-router-dom";
+import useAuth from "../../../Shared/Context/useAuth.js";
 
 export default function NotFound() {
   const navigate = useNavigate();
+
+  const { currentUser } = useAuth();
+  console.log("current user", currentUser);
+
+  const directionHandler = () => {
+    if (currentUser) {
+      navigate("/templates");
+    } else {
+      navigate("/");
+    }
+  };
   return (
     <>
       <div className="error-number">404</div>
@@ -12,7 +24,7 @@ export default function NotFound() {
       <h1 className="notfound-title">Oops! Page Not Found</h1>
 
       <div className="button-wrapper">
-        <button className="home-button" onClick={() => navigate("/")}>
+        <button className="home-button" onClick={directionHandler}>
           Take me Home
         </button>
       </div>
